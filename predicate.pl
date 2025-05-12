9e1082cb2d02f4cbad7cfaeaf9b89ec3133209e4
meglammata26 <meglammata7@gmail.com>
2025-05-12 16:21:10 -0500

    validated coordinates, found start location, and follow known path

--- predicate.pl
+++ predicate.pl
@@ -11,4 +11,24 @@ move_coord(coord(R,C), down, coord(R2,C)) :- R2 is R + 1.
 move_coord(coord(R,C), left, coord(R, C2)) :- C2 is C - 1.
 move_coord(coord(R,C), right, coord(R, C2)) :- C2 is C + 1. 
 
+% Making sure the coordinate is inside the maze
+valid_coord(Maze, coord(R,C)) :-
+    length(Maze, RowCount), R >= 0, R < RowCount, 
+    nth0(R, Maze, Row),
+    length(Row, ColCount), C >= 0, C < ColCount, 
+    cell(Maze, coord(R,C), V),
+    V \= w. 
+
+% Find the start location
+find_start(Maze, coord(R,C)) :-
+    nth0(R, Maze, Row),
+    nth0(C, Row, s).
+
+% Follow a known path
+follow_path(_, [], Coord, Coord).
+follow_path(Maze, [Action|Rest], Curr, End) :-
+    move_coord(Curr, Action, Next),
+    valid_coord(Maze, Next),
+    follow_path(Maze, Rest, Next, End).
+
 
